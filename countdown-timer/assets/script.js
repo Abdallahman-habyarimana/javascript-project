c4bf549ac8e01c330fccb2f3ce33061afce3bacc
Abdallahman Habyarimana <habdallah005@gmail.com>
2021-10-01 22:56:02 -0400

    countdown timer

---
+++ countdown-timer/assets/script.js
@@ -0,0 +1,27 @@
+let date = document.querySelector('.days');
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds')
+
+var futureDate = new Date('2021-10-29T11:30:00');
+var currentDate = new Date();
+
+function startCounting() {
+    const target = futureDate - currentDate;
+
+    const day = Math.floor(target / 1000 / 60 / 60 / 24)
+    const hour = Math.floor(target / 1000 / 60 / 60) % 24
+    const min = Math.floor(target / 1000 / 60) % 60
+    const sec = Math.floor(target / 1000) % 60
+    
+    date.innerHTML = day
+    hours.innerHTML = hour
+    minutes.innerHTML = min
+    seconds.innerHTML = sec
+}
+
+setInterval(startCounting, 1000);
+
+
+
+
