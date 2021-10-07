050aef203c4bb28cbfef2c8cad0ef7fb00db5a7d
Abdallahman Habyarimana <habdallah005@gmail.com>
2021-10-06 22:07:41 -0400

    Adding animation

--- projectiles-game/assets/script.js
+++ projectiles-game/assets/script.js
@@ -142,12 +142,22 @@ const animate = () => {
 
     projectiles.forEach((projectile, projectileIndex) => {
     const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
-       if(dist - enemy.radius - projectile.radius < 1 ){
+    // when projectiles touch enemy   
+    if(dist - enemy.radius - projectile.radius < 1 ){
+      if(enemy.radius - 10 > 5)   {
+        gsap.to(enemy, {
+          radius: enemy.radius - 10
+        })
+        setTimeout(() => {
+          projectiles.splice(projectileIndex, 1);
+        }, 0); 
+      } else {
         setTimeout(() => {
           enemies.splice(index, 1);
           projectiles.splice(projectileIndex, 1);
- 
-        }, 0);   
+        }, 0);  
+      }
+       
       }
   })
   })
